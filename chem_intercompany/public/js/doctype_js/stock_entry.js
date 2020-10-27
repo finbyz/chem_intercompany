ff916bd2b91314e89af1e5066fbcc62c3cba9f38
finbyz <info@finbyz.com>
2020-10-27 08:39:17 +0000

    SE js changes

--- chem_intercompany/public/js/doctype_js/stock_entry.js
+++ chem_intercompany/public/js/doctype_js/stock_entry.js
@@ -19,7 +19,9 @@ frappe.ui.form.on('Stock Entry', {
                 }
             }
         };
-        
+        if (frm.doc.docstatus == 0){
+            frm.trigger('stock_entry_type')
+        }  
     },
     refresh: function(frm){
         if (frm.doc.amended_from && frm.doc.__islocal && frm.doc.docstatus == 0) {
@@ -37,6 +39,85 @@ frappe.ui.form.on('Stock Entry', {
     stock_entry_type: function (frm) {
         if(frm.doc.stock_entry_type == "Send Jobwork Finish") {
             frm.set_value('from_bom',1)
+        }
+        if(frm.doc.purpose == "Material Receipt" || frm.doc.purpose == "Repack"){
+            frappe.meta.get_docfield("Stock Entry Detail","receive_packing_size", cur_frm.doc.name).read_only = 0;
+
+            frappe.meta.get_docfield("Stock Entry Detail","tare_weight", cur_frm.doc.name).read_only = 0;
+
+            frappe.meta.get_docfield("Stock Entry Detail","receive_no_of_packages", cur_frm.doc.name).read_only = 0;
+
+            frappe.meta.get_docfield("Stock Entry Detail","received_qty", cur_frm.doc.name).read_only = 0;
+
+
+            frappe.meta.get_docfield("Stock Entry Detail","receive_qty", cur_frm.doc.name).read_only = 0;
+
+            frappe.meta.get_docfield("Stock Entry Detail","received_concentration", cur_frm.doc.name).read_only = 0;
+
+            frappe.meta.get_docfield("Stock Entry Detail","receive_quantity", cur_frm.doc.name).read_only = 0;
+
+            frappe.meta.get_docfield("Stock Entry Detail","supplier_no_of_packages", cur_frm.doc.name).read_only = 0;
+
+            frappe.meta.get_docfield("Stock Entry Detail","supplier_packing_size", cur_frm.doc.name).read_only = 0;
+
+            frappe.meta.get_docfield("Stock Entry Detail","supplier_concentration", cur_frm.doc.name).read_only = 0;
+
+            frappe.meta.get_docfield("Stock Entry Detail","supplier_qty", cur_frm.doc.name).read_only = 0;
+
+            frappe.meta.get_docfield("Stock Entry Detail","supplier_quantity", cur_frm.doc.name).read_only = 0;
+
+            frappe.meta.get_docfield("Stock Entry Detail","accepted_qty", cur_frm.doc.name).read_only = 0;
+
+            frappe.meta.get_docfield("Stock Entry Detail","accepted_concentration", cur_frm.doc.name).read_only = 0;
+
+            frappe.meta.get_docfield("Stock Entry Detail","accepted_quantity", cur_frm.doc.name).read_only = 0;
+
+            frappe.meta.get_docfield("Stock Entry Detail","accepted_packing_size", cur_frm.doc.name).read_only = 0;
+
+            frappe.meta.get_docfield("Stock Entry Detail","accepted_no_of_packages", cur_frm.doc.name).read_only = 0;
+            
+            frappe.meta.get_docfield("Stock Entry Detail","short_quantity", cur_frm.doc.name).read_only = 0;
+            
+            frappe.meta.get_docfield("Stock Entry Detail","amount_difference", cur_frm.doc.name).read_only = 0;
+        }
+        else{
+            frappe.meta.get_docfield("Stock Entry Detail","receive_packing_size", cur_frm.doc.name).read_only = 1;
+
+            frappe.meta.get_docfield("Stock Entry Detail","tare_weight", cur_frm.doc.name).read_only = 1;
+
+            frappe.meta.get_docfield("Stock Entry Detail","receive_no_of_packages", cur_frm.doc.name).read_only = 1;
+
+            frappe.meta.get_docfield("Stock Entry Detail","received_qty", cur_frm.doc.name).read_only = 1;
+
+            frappe.meta.get_docfield("Stock Entry Detail","receive_qty", cur_frm.doc.name).read_only = 1;
+
+            frappe.meta.get_docfield("Stock Entry Detail","received_concentration", cur_frm.doc.name).read_only = 1;
+
+            frappe.meta.get_docfield("Stock Entry Detail","receive_quantity", cur_frm.doc.name).read_only = 1;
+
+            frappe.meta.get_docfield("Stock Entry Detail","supplier_no_of_packages", cur_frm.doc.name).read_only = 1;
+
+            frappe.meta.get_docfield("Stock Entry Detail","supplier_packing_size", cur_frm.doc.name).read_only = 1;
+
+            frappe.meta.get_docfield("Stock Entry Detail","supplier_concentration", cur_frm.doc.name).read_only = 1;
+
+            frappe.meta.get_docfield("Stock Entry Detail","supplier_qty", cur_frm.doc.name).read_only = 1;
+
+            frappe.meta.get_docfield("Stock Entry Detail","supplier_quantity", cur_frm.doc.name).read_only = 1;
+
+            frappe.meta.get_docfield("Stock Entry Detail","accepted_qty", cur_frm.doc.name).read_only = 1;
+
+            frappe.meta.get_docfield("Stock Entry Detail","accepted_concentration", cur_frm.doc.name).read_only = 1;
+
+            frappe.meta.get_docfield("Stock Entry Detail","accepted_quantity", cur_frm.doc.name).read_only = 1;
+
+            frappe.meta.get_docfield("Stock Entry Detail","accepted_packing_size", cur_frm.doc.name).read_only = 1;
+
+            frappe.meta.get_docfield("Stock Entry Detail","accepted_no_of_packages", cur_frm.doc.name).read_only = 1;
+
+            frappe.meta.get_docfield("Stock Entry Detail","short_quantity", cur_frm.doc.name).read_only = 1;
+
+            frappe.meta.get_docfield("Stock Entry Detail","amount_difference", cur_frm.doc.name).read_only = 1;
         }
     },
     get_bom_items: function(frm){
