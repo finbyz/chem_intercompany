16ff23062845389c459451c9de12da238c7a89f5
finbyz <info@finbyz.com>
2020-11-05 06:14:12 +0000

    Jobwork changes

--- chem_intercompany/controllers/batch_controller.py
+++ chem_intercompany/controllers/batch_controller.py
@@ -4,12 +4,12 @@ from frappe import _
 from frappe.utils import cstr
 
 @frappe.whitelist()
-def get_fifo_batches(item_code, warehouse, party):
+def get_fifo_batches(item_code, warehouse, party, posting_date, posting_time):
 	batches = frappe.db.sql("""
 		select 
 			bt.batch_id, sum(sle.actual_qty) as qty, bt.concentration
-        from `tabBatch` as bt
-        join `tabStock Ledger Entry` as sle ignore index (item_code, warehouse) 
+		from `tabBatch` as bt
+		join `tabStock Ledger Entry` as sle ignore index (item_code, warehouse) 
 		on (bt.batch_id = sle.batch_no )
 		JOIN `tabStock Entry` as se ON se.name = sle.voucher_no
 		where 
@@ -17,8 +17,25 @@ def get_fifo_batches(item_code, warehouse, party):
 			and sle.warehouse = %s 
 			and (bt.expiry_date >= CURDATE() or bt.expiry_date IS NULL)
 			and se.party = %s
+			and concat(sle.posting_date, ' ', sle.posting_time) <= %s %s
 		group by sle.batch_no
 		having sum(sle.actual_qty) > 0 
-		order by sle.posting_date, bt.name """, (item_code, warehouse, party), as_dict=True)
+		order by sle.posting_date, bt.name """, (item_code, warehouse, party, posting_date, posting_time), as_dict=True)
 
 	return batches
+
+def get_qty_from_sle(item_code, warehouse, party, posting_date, posting_time):
+	qty = frappe.db.sql("""
+	select 
+		sum(sle.actual_qty) as qty
+	from 
+		`tabStock Ledger Entry` as sle
+	JOIN `tabStock Entry` as se ON se.name = sle.voucher_no
+	where 
+		item_code=%s and warehouse=%s and se.party = %s 
+		and concat(sle.posting_date, ' ', sle.posting_time) <= %s %s""", (item_code, warehouse, party, posting_date, posting_time))[0][0]
+
+	if qty!=None:
+		return qty
+	else:
+		return 0
