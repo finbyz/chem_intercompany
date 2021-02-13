c432d90ae0263f93b0b5cb22208ed766347f63f9
finbyz <info@finbyz.com>
2021-02-13 10:51:14 +0000

    remove row if not found batch

--- chem_intercompany/chem_intercompany/doc_events/stock_entry.py
+++ chem_intercompany/chem_intercompany/doc_events/stock_entry.py
@@ -385,7 +385,8 @@ def job_work_item_reset(self,job_work_out_warehouse,party):
 			if not batches:
 				if not self.allow_short_qty_consumption:
 					frappe.throw(_("Sufficient quantity for item {} is not available in {} warehouse for party {}.".format(frappe.bold(d.item_code), frappe.bold(d.s_warehouse),party)))
-					
+				else:
+					to_remove.append(d)	
 			for batch in batches:	
 				if batch.batch_id == d.batch_no or batch.lot_no == d.lot_no:
 					batch_qty_dict.update({batch.batch_id:batch.qty})		
@@ -405,6 +406,7 @@ def job_work_item_reset(self,job_work_out_warehouse,party):
 				remaining_quantity = round(flt(d.qty),2)
 
 			i = 0
+			#frappe.msgprint("last: " + str(batch_qty_dict))
 			
 			for batch, qty in batch_qty_dict.items():
 				# frappe.msgprint(str(batch) + " : " +str(qty))
@@ -510,8 +512,7 @@ def job_work_item_reset(self,job_work_out_warehouse,party):
 				#frappe.msgprint(str(remaining_quantity))
 				if round_down(remaining_quantity,1):
 					if self.allow_short_qty_consumption:
-						to_remove.append(d)
-						frappe.msgprint(_("Sufficient quantity for item {} is not available in {} warehouse for party {}.".format(frappe.bold(d.item_code), frappe.bold(d.s_warehouse), party)))
+						frappe.msgprint(_("_Sufficient quantity for item {} is not available in {} warehouse for party {}.".format(frappe.bold(d.item_code), frappe.bold(d.s_warehouse), party)))
 					else:
 						frappe.throw(_("Sufficient quantity for item {} is not available in {} warehouse for party {}.".format(frappe.bold(d.item_code), frappe.bold(d.s_warehouse), party)))
 			
