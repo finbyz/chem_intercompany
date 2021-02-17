48bba15aa95dfc76648c73742918b793cc0ce1c2
finbyz <info@finbyz.com>
2021-02-17 08:49:14 +0000

    job work

--- chem_intercompany/chem_intercompany/doc_events/stock_entry.py
+++ chem_intercompany/chem_intercompany/doc_events/stock_entry.py
@@ -398,7 +398,15 @@ def job_work_item_reset(self,job_work_out_warehouse,party):
 			for batch, qty in batch_utilized.items():
 				if batch_qty_dict.get(batch):
 					batch_qty_dict[batch] = (flt(batch_qty_dict[batch]) - flt(qty))
-			
+					if batch_qty_dict[batch] == 0:
+						batch_qty_dict.pop(batch)
+
+			if batch_qty_dict == {}:
+				if not self.allow_short_qty_consumption:
+					frappe.throw(_("Sufficient quantity for item {} is not available in {} warehouse for party {}.".format(frappe.bold(d.item_code), frappe.bold(d.s_warehouse),party)))
+				else:
+					to_remove.append(d)
+												
 			concentration = d.concentration or 100
 			if maintain_as_is_stock:	
 				remaining_quantity = round(flt(d.qty)*flt(concentration)/100,2)
